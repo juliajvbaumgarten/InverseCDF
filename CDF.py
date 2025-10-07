530e8a770803c253120f2eadf57678e7cfcf61ab
juliajvbaumgarten <juliaxbaumgarten@gmail.com>
2025-10-07 12:48:32 -0400

    inverse CDF

--- CDF.py
+++ CDF.py
@@ -4,3 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Inverse CDF Part
+# Truncated exponential on [0,1] with shape beta > 0:
+# Inverse CDF: x = - (1/beta) * ln(1 - u*(1 - exp(-beta)))
+
+
+
