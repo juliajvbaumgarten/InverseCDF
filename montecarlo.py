a237686088314a93cc4bb32260dcbe23b911c243
lsdun <lasdun@syr.edu>
2025-10-09 13:15:30 -0400

    Update montecarlo.py

--- montecarlo.py
+++ montecarlo.py
@@ -15,7 +15,7 @@ def mc_area_hit_or_miss(a, b, N, rng=np.random.default_rng()):
     # Binomial variance for hit/miss:
     var_p = p * (1 - p) / N
     std = box_area * np.sqrt(var_p) # 1sigma statistical uncertainty
-    return est, std
+    return est, std # return estimated area and statistical uncertainty
 
 def mc_area_1d(a, b, N, rng=np.random.default_rng()):
     x = rng.uniform(-a, a, size=N)
@@ -24,7 +24,7 @@ def mc_area_1d(a, b, N, rng=np.random.default_rng()):
     vals = integrand
     est = width * vals.mean()
     std = width * (vals.std(ddof=1) / np.sqrt(N))
-    return est, std
+    return est, std # return estimated area and statistical uncertainty
 
 def mc_circumference(a, b, N, rng=np.random.default_rng()):
     # C = ∫_0^{2π} sqrt(a^2 sin^2 θ + b^2 cos^2 θ) dθ
@@ -32,7 +32,7 @@ def mc_circumference(a, b, N, rng=np.random.default_rng()):
     vals = np.sqrt((a ** 2) * (np.sin(theta) ** 2) + (b ** 2) * (np.cos(theta) ** 2))
     est = (2.0 * np.pi) * vals.mean()
     std = (2.0 * np.pi) * (vals.std(ddof=1) / np.sqrt(N))
-    return est, std
+    return est, std # return estimated area and statistical uncertainty
 
 def circumference_ramanujan(a, b):
     h = ((a - b) ** 2) / ((a + b) ** 2)
@@ -43,5 +43,15 @@ def circumference_ramanujan(a, b):
 a = 3.0
 b = 2.0
 
+Ns = np.arange(1000, 10001, 1000)
+
+estimated_area = []
+
+for N in Ns:
+    est, std = mc_area_hit_or_miss(a, b, N)
+    estimated_area.append(est)
+    stat_unc.append(std)
+
+estimated_area = np.array(estimated_area)
 
 
